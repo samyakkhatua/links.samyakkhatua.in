aefd177d7f203068e7b93df0b4053eaab9888521
samyakkhatua <samyakkhatua@duck.com>
2023-07-07 23:24:21 +0530

    desc changed

--- src/App.jsx
+++ src/App.jsx
@@ -37,15 +37,15 @@ function App() {
         </div>
 
         <a href="#" class="inline-flex justify-between items-center py-1 px-1 pr-4 mb-4 text-sm text-gray-700 bg-gray-100 rounded-full w-full hover:bg-gray-200" role="alert">
-            <span class="text-xs bg-blue-600 rounded-full text-white px-4 py-1.5 mr-3 w-30">Threads</span> <span class="text-sm font-medium">Threads is out! See what's happening!</span> 
+            <span class="text-xs bg-blue-600 rounded-full text-white px-4 py-1.5 mr-3 w-30">Threads</span> <span class="text-sm font-medium">Let's start the conversations!</span> 
             <svg class="ml-2 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd"></path></svg>
         </a>
         <a href="#" class="inline-flex justify-between items-center py-1 px-1 pr-4 mb-4 text-sm text-gray-700 bg-gray-100 rounded-full w-full hover:bg-gray-200" role="alert">
-            <span class="text-xs bg-blue-600 rounded-full text-white px-4 py-1.5 mr-3 w-30">Linkedin</span> <span class="text-sm font-medium">Threads is out! See what's happening!</span> 
+            <span class="text-xs bg-blue-600 rounded-full text-white px-4 py-1.5 mr-3 w-30">Linkedin</span> <span class="text-sm font-medium">Let's connect and network!</span> 
             <svg class="ml-2 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd"></path></svg>
         </a>
         <a href="#" class="inline-flex justify-between items-center py-1 px-1 pr-4 mb-4 text-sm text-gray-700 bg-gray-100 rounded-full w-full hover:bg-gray-200" role="alert">
-            <span class="text-xs bg-blue-600 rounded-full text-white px-4 py-1.5 mr-3 w-30">Github</span> <span class="text-sm font-medium">Threads is out! See what's happening!</span> 
+            <span class="text-xs bg-blue-600 rounded-full text-white px-4 py-1.5 mr-3 w-30">Github</span> <span class="text-sm font-medium">Explore projects and repositories!</span> 
             <svg class="ml-2 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd"></path></svg>
         </a>
 
