e8d4bb984d4fdb7c95e5f50430f9c2fb1c407245
samyakkhatua <samyakkhatua@duck.com>
2023-07-07 23:36:07 +0530

    Links added

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,20 @@ import "./App.css";
 function App() {
   return (
     <div className="">
-      <div className="justify-content m-auto w-[50%] ">
-        
-
+      <div className="justify-content m-auto w-full sm:w-[40%] ">
         <div class="gap-6 text-center md:grid-cols-3 lg:gap-12 text-white">
           <div class="mb-12 md:mb-0">
             <div class="mb-6 flex justify-center">
               <img
-                src="https://tecdn.b-cdn.net/img/Photos/Avatars/img%20(1).jpg"
+                src="public/profile-pic.png"
                 class="w-32 rounded-full shadow-lg dark:shadow-black/30"
               />
             </div>
 
-            <h5 class="mb-2 text-4xl font-extrabold tracking-tight leading-none text-white md:text-4xl lg:text-4xl">Samyak Khatua</h5>
-            
+            <h5 class="mb-2 text-4xl font-extrabold tracking-tight leading-none text-white md:text-4xl lg:text-4xl">
+              Samyak Khatua
+            </h5>
+
             <h6 class="mt-0 mb-8 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48">
               Fullstack Developer
             </h6>
@@ -36,23 +36,84 @@ function App() {
           </div>
         </div>
 
-        <a href="#" class="inline-flex justify-between items-center py-1 px-1 pr-4 mb-4 text-sm text-gray-700 bg-gray-100 rounded-full w-full hover:bg-gray-200" role="alert">
-            <span class="text-xs bg-blue-600 rounded-full text-white px-4 py-1.5 mr-3 w-30">Threads</span> <span class="text-sm font-medium">Let's start the conversations!</span> 
-            <svg class="ml-2 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd"></path></svg>
-        </a>
-        <a href="#" class="inline-flex justify-between items-center py-1 px-1 pr-4 mb-4 text-sm text-gray-700 bg-gray-100 rounded-full w-full hover:bg-gray-200" role="alert">
-            <span class="text-xs bg-blue-600 rounded-full text-white px-4 py-1.5 mr-3 w-30">Linkedin</span> <span class="text-sm font-medium">Let's connect and network!</span> 
-            <svg class="ml-2 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd"></path></svg>
-        </a>
-        <a href="#" class="inline-flex justify-between items-center py-1 px-1 pr-4 mb-4 text-sm text-gray-700 bg-gray-100 rounded-full w-full hover:bg-gray-200" role="alert">
-            <span class="text-xs bg-blue-600 rounded-full text-white px-4 py-1.5 mr-3 w-30">Github</span> <span class="text-sm font-medium">Explore projects and repositories!</span> 
-            <svg class="ml-2 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd"></path></svg>
+        {/* Threads */}
+        <a
+          href="https://bit.ly/3pufGl6"
+          class="inline-flex justify-between items-center py-1 px-1 pr-4 mb-4 text-sm text-gray-700 bg-gray-100 rounded-full w-full hover:bg-gray-200 sm:w-full"
+          role="alert"
+          target="_blank"
+        >
+          <span class="text-xs bg-blue-600 rounded-full text-white px-4 py-1.5 mr-3 ">
+            Threads
+          </span>{" "}
+          <span class="text-sm font-medium">
+            Let's start the conversations!
+          </span>
+          <svg
+            class="ml-2 w-5 h-5"
+            fill="currentColor"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+              clip-rule="evenodd"
+            ></path>
+          </svg>
         </a>
 
+        {/* Linkedin */}
+        <a
+          href="https://bit.ly/3hdZJL8"
+          class="inline-flex justify-between items-center py-1 px-1 pr-4 mb-4 text-sm text-gray-700 bg-gray-100 rounded-full w-full hover:bg-gray-200"
+          role="alert"
+          target="_blank"
+        >
+          <span class="text-xs bg-blue-600 rounded-full text-white px-4 py-1.5 mr-3 w-30">
+            Linkedin
+          </span>{" "}
+          <span class="text-sm font-medium">Let's connect and network!</span>
+          <svg
+            class="ml-2 w-5 h-5"
+            fill="currentColor"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+              clip-rule="evenodd"
+            ></path>
+          </svg>
+        </a>
 
-        
-
-        
+        {/* Github */}
+        <a
+          href="https://bit.ly/3hez3Kl"
+          class="inline-flex justify-between items-center py-1 px-1 pr-4 mb-4 text-sm text-gray-700 bg-gray-100 rounded-full w-full hover:bg-gray-200"
+          role="alert"
+          target="_blank"
+        >
+          <span class="text-xs bg-blue-600 rounded-full text-white px-4 py-1.5 mr-3 w-30">
+            Github
+          </span>{" "}
+          <span class="text-sm font-medium">
+            Explore projects and repositories!
+          </span>
+          <svg
+            class="ml-2 w-5 h-5"
+            fill="currentColor"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+              clip-rule="evenodd"
+            ></path>
+          </svg>
+        </a>
       </div>
     </div>
   );
