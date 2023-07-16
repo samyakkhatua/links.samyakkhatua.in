e90f37e7dc2e2094ddd49470103e5756634214f7
samyakkhatua <samyakkhatua@duck.com>
2023-07-16 18:53:40 +0530

    Email card added

--- src/App.jsx
+++ src/App.jsx
@@ -126,6 +126,34 @@ function App() {
             ></path>
           </svg>
         </a>
+
+        {/* Mail */}
+        <a
+          href="mailto:hello@samyakkhatua.in"
+          class="inline-flex justify-between items-center py-1 px-1 pr-4 mb-4 text-sm text-gray-700 bg-gray-100 rounded-full w-full hover:bg-gray-200"
+          role="alert"
+          target="_blank"
+          onClick={() => gaEventTracker('github')}
+        >
+          <span class="text-xs bg-blue-600 rounded-full text-white px-4 py-1.5 mr-3 w-30">
+            Email
+          </span>{" "}
+          <span class="text-sm font-medium">
+          hello@samyakkhatua.in
+          </span>
+          <svg
+            class="ml-2 w-5 h-5"
+            fill="currentColor"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+              clip-rule="evenodd"
+            ></path>
+          </svg>
+        </a>
       </div>
       
       <p class="w-full text-center my-12 text-gray-600 sticky pt-32 bottom-0">Made by Samyak Khatua</p>
