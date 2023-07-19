3e6bf6b736bfa9461c4662deb6e7e4855f67f0e0
samyakkhatua <samyakkhatua@duck.com>
2023-07-19 16:04:11 +0530

    widget footer

--- src/components/CurrentlyPlaying.jsx
+++ src/components/CurrentlyPlaying.jsx
@@ -36,7 +36,7 @@ function CurrentlyPlaying() {
             />
 
             <div className="flex items-center justify-between px-4 py-2 bg-green-400">
-              <h1 className="text-lg font-bold text-white"></h1>
+            <h1 className="text-xs">Widget by SpotiNow</h1>
               <a href={currentlyPlaying.link}>
                 <button className="px-2 py-1 text-xs font-semibold text-gray-900 uppercase transition-colors duration-300 transform bg-white rounded hover:bg-gray-200 focus:bg-gray-400 focus:outline-none">
                   Listen Now
@@ -69,7 +69,7 @@ function CurrentlyPlaying() {
             />
 
             <div className="flex items-center justify-between px-4 py-2 bg-green-400">
-              <h1 className="text-lg font-bold text-white"></h1>
+              <h1 className="text-xs">Widget by SpotiNow</h1>
               <a href="https://github.com/samyakkhatua/SpotiNow">
                 <button className="px-2 py-1 text-xs font-semibold text-gray-900 uppercase transition-colors duration-300 transform bg-white rounded hover:bg-gray-200 focus:bg-gray-400 focus:outline-none">
                 Project Link
