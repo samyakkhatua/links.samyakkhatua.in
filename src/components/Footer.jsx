8b7c1c5de6478367c2c984c63411fdc5788f3518
samyakkhatua <samyakkhatua@duck.com>
2023-07-20 22:49:44 +0530

    Footer component introduced

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="">
+      <p class="w-full text-center my-12 text-gray-600 pt-32 bottom-0">
+        Made by Samyak Khatua
+      </p>
+    </div>
+  );
+};
+
+export default Footer;
