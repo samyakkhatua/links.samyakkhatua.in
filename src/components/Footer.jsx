c41e581657e19bca59e43cd495895e6d2d229d64
samyakkhatua <samyakkhatua@duck.com>
2023-08-01 23:18:54 +0530

    github link added to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,9 +3,11 @@ import React from "react";
 const Footer = () => {
   return (
     <div className="">
-      <p class="w-full text-center my-12 text-gray-600 pt-32 bottom-0">
-        Made by Samyak Khatua
-      </p>
+      <a href="https://bit.ly/3hez3Kl">
+        <p class="w-full text-center my-12 text-gray-600 pt-32 bottom-0">
+          Made by Samyak Khatua
+        </p>
+      </a>
     </div>
   );
 };
